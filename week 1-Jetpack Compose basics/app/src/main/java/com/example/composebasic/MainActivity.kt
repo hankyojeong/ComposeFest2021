a1bafa6cf9b04026ed9c41b8dc5903f557592b8c
hankyojeong <tkdydwkdlfma90@gmail.com>
2021-11-08 09:00:53 -0800

    feat: Add First Week Tutorial Compose Android Project

---
+++ week 1-Jetpack Compose basics/app/src/main/java/com/example/composebasic/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.composebasic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composebasic.ui.theme.ComposeBasicTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+//            Text("Hello World");
+            MessageCard(name = "Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+   ComposeBasicTheme {
+       MessageCard(name = "Android")
+   } 
+}
